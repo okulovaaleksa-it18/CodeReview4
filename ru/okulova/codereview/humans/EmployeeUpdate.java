129979fd7641fff07d9f67f857e1fd8270d7898c
Aleksandra Okulova <sasaokulova055@gmail.com>
2026-04-20 22:43:12 +0500

    добавление

---
+++ ru/okulova/codereview/humans/EmployeeUpdate.java
@@ -0,0 +1,42 @@
+package ru.okulova.codereview.humans;
+
+import java.util.List;
+
+public class EmployeeUpdate {
+    private String name;
+    private Department department;
+
+    public EmployeeUpdate(String name, Department department) {
+        this.name = name;
+        this.department = department;
+        if (department != null) {
+            department.addEmployee(this);
+        }
+    }
+
+    public Department getDepartment() {
+        return department;
+    }
+    public boolean isBoss() {
+        return this == department.getBoss();
+    }
+    public List<EmployeeUpdate> getDepartmentEmployees() {
+        return department.getEmployees();
+    }
+    public String getName() {
+        return name;
+    }
+
+    @Override
+    public String toString() {
+        String bossName;
+        //FIX_ME: убрали дублирование логики
+        //if (department.getBoss() == this) {
+        if (isBoss()) {
+            return name + " - начальник отдела " + department.getName();
+        }
+        return name + " работает в отделе " + department.getName() +
+                ", начальник - " +
+                (department.getBoss() != null ? department.getBoss().getName() : "не назначен");
+    }
+}
---
+++ ru/okulova/codereview/time/Time.java
@@ -0,0 +1,38 @@
+package ru.okulova.codereview.time;
+
+public class Time {
+    private int time;
+    private int originalTime;
+
+    public Time() {
+        this.time = 0;
+    }
+
+    public Time(int time) {
+        setTime(time);
+    }
+
+    public void setTime(int time) {
+        this.originalTime = time;
+        this.time = (time%86400 + 86400)%86400;
+    }
+
+    private int getHours() {
+        return time/3600;
+    }
+
+    private int getMinutes() {
+        return (time%3600)/60;
+    }
+
+    private int getSeconds() {
+        return time%60;
+    }
+
+    @Override
+    public String toString() {
+        return "Время для " + originalTime + " секунд : "
+                + getHours() + " : " + getMinutes() + " : "
+                + getSeconds() + " ";
+    }
+}
